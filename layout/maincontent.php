1a941106a5b01009238dfefc0c7dd2485cd77e38
rissajune <75287649+rissajune@users.noreply.github.com>
2021-09-23 12:48:59 -0500

    Update maincontent.php

--- layout/maincontent.php
+++ layout/maincontent.php
@@ -3,7 +3,7 @@
             <div id="mainContent">
                 <h2>This is what an engineer looks like</h2>
                 <a href="imgs/selfie"><img src="imgs/selfie/selfie.jpg"  width="350" heigth="400" alt="woman dressed in pink looking at camera"/></a>
-                <p> Hi, I'm Rissa! I'm a technical writer and copy editor with a demonstrated history of working in the tech industry. Need a custom website? A social media manager? I would love to help you help your small business rise from ashes today.</p>            
+                <p> Hi, I'm Rissa! I'm a sarcastic dog-mom with a passion for cruelty-free cosmetics. I don't do anything except my laundry halfway. I need the oxford comma like I need my weighted blanket to sleep at night. I'm a technical writer and copy editor with a demonstrated history of working in the tech industry. Need a custom website? A social media manager? I would love to help you help your small business rise from ashes today.</p>            
                  <a href="imgs/flower"><img src="imgs/flower/flower.jpg"  width="400" heigth="400" alt="woman in flowerbed"/></a>
                <br />
                <br />
